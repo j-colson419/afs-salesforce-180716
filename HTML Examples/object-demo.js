1c06d78f2982e0d4b3278b4ebad42d1ec43378ee
Madrid-Ortega <m.madrid-ortega@accenturefederal.com>
2018-08-09 10:19:58 -0400

    adding html css and js and notes

--- HTML Examples/object-demo.js
+++ HTML Examples/object-demo.js
@@ -1,5 +1,5 @@
 function main(){
-    console.log("my person: " + p)
+    console.log(p);
 }
 
 function Person(name, age){
@@ -7,4 +7,22 @@ function Person(name, age){
     this.age = age;
 }
 
-var p = new Person("Marissa", 22);
+var p = new Person("Jess", 45);
+
+document.getElementById('create').addEventListener('click', function(e){
+
+    var name = document.getElementById('name').value;
+
+    console.log(document.getElementById('name'));
+
+    var age = document.getElementById('age').value;
+
+    console.log(document.getElementById('age'));
+
+    var p2 = new Person(name, age);
+
+    console.log(p2);
+
+    document.getElementById('name').value = "Hi";
+
+}, false)
