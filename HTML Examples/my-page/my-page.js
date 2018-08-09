69425ff18c9a29aa222bf6d235dda063ea8bac47
Greenboigz <smgrnbrg91495@gmail.com>
2018-08-09 16:58:00 -0400

    Made small revisions to my-page

--- HTML Examples/my-page/my-page.js
+++ HTML Examples/my-page/my-page.js
@@ -1,12 +1,19 @@
+/**
+ * I got help on creating the masonry style card layout from this article on medium.
+ * It utilizes a combination of css styling and javascript code to dynamically update
+ * the location and grid-row-end properties of the cards.
+ * 
+ * @source https://medium.com/@andybarefoot/a-masonry-style-layout-using-css-grid-8c663d355ebb
+ */
+
 function resizeGridCard(card){
     grid = document.getElementsByClassName("grid")[0];
     rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
     rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
     rowSpan = Math.ceil(
-        (card.querySelector('.card-content').getBoundingClientRect().height + rowGap) / 
+        (card.querySelector('.card-content').getBoundingClientRect().height + 4 * rowGap) / 
         (rowHeight + rowGap)
     );
-    console.log(rowSpan);
     card.style.gridRowEnd = "span "+rowSpan;
 }
 
